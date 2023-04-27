e615832d482c29802b3569e47a097a004df6345f
kainianc1998 <kainianc@gmail.com>
2023-04-27 20:51:29 +0900

    Add exercise

---
+++ LeetCode/Longest_Palindromic_Substring/main.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        resLen = 0
+
+        for pivit in range(len(s)):
+            # odd len(s) case
+            left, right = pivit, pivit
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > resLen:
+                    res = s[left : right + 1]
+                    resLen = right - left + 1
+                left -= 1
+                right += 1
+            # even len(s) case
+            left, right = pivit, pivit+1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > resLen:
+                    res = s[left : right + 1]
+                    resLen = right - left + 1
+                left -= 1
+                right += 1
+        return res
